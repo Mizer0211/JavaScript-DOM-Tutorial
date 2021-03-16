3563757c0aa7aef04ed6af36cf655fe9f242fcff
Mizer0211 <Sanjok60660@gmail.com>
2021-03-16 16:02:03 +0200

    video12

--- app.js
+++ app.js
@@ -10,9 +10,23 @@ list.addEventListener('click', function(e){
 
 // add books-list
 const addForm = document.forms['add-book'];
-
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // appened to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+
 });
